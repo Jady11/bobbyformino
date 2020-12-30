567f5ff49b8931d6df9d175728bf1889ff2e8dc1
Dustin LaMarr <dustin.lamarr@gmail.com>
2020-12-30 10:32:54 -0700

    Added render functionality for home page.

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 var path = require("path");
+const helpers = require('./utils/helpers');
 
 var dir = path.join(__dirname, 'public');
 
@@ -11,7 +13,13 @@ const { sequelize } = require('./models/index');
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+const hbs = exphbs.create({ helpers });
+
 // app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
